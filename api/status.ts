6db44222c214274801dee2441f9c4298b8174fce
CF <masone@users.noreply.github.com>
2025-12-23 13:49:26 +0100

    is hell UI

--- api/status.ts
+++ api/status.ts
@@ -8,8 +8,10 @@ export default async function handler(
   try {
     const result = await isHell();
     res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader("Cache-Control", "s-maxage=300, stale-while-revalidate");
+    res.setHeader("Content-Type", "application/json");    
+    if(process.env.NODE_ENV === "production"){
+      res.setHeader("Cache-Control", "s-maxage=300, stale-while-revalidate");
+    }
     res.end(JSON.stringify({ hell: result }));
   } catch (err) {
     const message = err instanceof Error ? err.message : String(err);
